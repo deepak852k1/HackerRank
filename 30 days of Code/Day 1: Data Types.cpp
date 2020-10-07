bd49160e5ad59d315ead6ffc730f5f2579ef8dc6
Deepak <54444824+deepak852k1@users.noreply.github.com>
2020-10-07 22:43:42 +0530

    Create Day 1: Data Types.cpp
    Day 1: Data Types

---
+++ 30 days of Code/Day 1: Data Types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    return 0;
+}
