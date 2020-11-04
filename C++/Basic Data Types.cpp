b8ea84a34776c61a9cff1ff4e0122f544075a4de
Deepak <54444824+deepak852k1@users.noreply.github.com>
2020-11-04 20:22:19 +0530

    Basic Data Types
    Basic Data Types

---
+++ C++/Basic Data Types.cpp
@@ -0,0 +1,22 @@
+Problem Link:
+https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem
+
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+      // Complete the code.
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n";
+    cout<<fixed<<setprecision(3)<<d<<"\n";
+    cout<<fixed<<setprecision(9)<<e<<"\n";
+    return 0;
+}
