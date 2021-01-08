a204945a3b2284c7269af8dc84b2b1bdf97005a6
Deepak <54444824+deepak852k1@users.noreply.github.com>
2021-01-08 23:22:52 +0530

    Update "Hello World!" in C.c

--- C/"Hello World!" in C.c
+++ C/"Hello World!" in C.c
@@ -7,9 +7,7 @@ int main()
 {
 	
     char s[100];
-    scanf("%[^\n]%*c", &s);
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    scanf("%[^\n]%*c", &s);  
     printf("Hello, World!\n");
     printf("Welcome to C programming.");
     return 0;
